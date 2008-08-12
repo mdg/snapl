5a8d6774300eaf9ab46140e5cff1d8ed22b56813
Matt Graham <mdg149@optonline.net>
2008-08-12 00:57:11 -0400

    added the session_id check to the request test

--- test/request_test.cpp
+++ test/request_test.cpp
@@ -8,7 +8,6 @@ TESTPP( test_constructor_1 )
 	request_c req( RT_SESSION_STATUS, "dog" );
 	RT_SESSION_STATUS == actual( req.request_type() );
 	std::string( "dog" ) == actual( req.session_id() );
-	// "dog" == actual( req.session_id() );
 }
 
 TESTPP( test_constructor_2 )
