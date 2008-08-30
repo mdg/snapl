f640a3343a833d0eb1e1e62b7a225b5fc5d87216
Matt Graham <mdg149@optonline.net>
2008-08-30 18:01:54 -0400

    fixed another compile error from the connection.read/write change that was hidden by missing header dependencies in the Rakefile

--- src/shession_client.cpp
+++ src/shession_client.cpp
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2008 Matthew Graham
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "shession_client.h"
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -8,8 +23,7 @@
 
 shession_client_c::shession_client_c()
 : m_connection()
-{
-}
+{}
 
 shession_client_c::~shession_client_c()
 {
@@ -61,7 +75,7 @@ bool shession_client_c::live_session( const std::string &session_id )
 	write_request( "status", session_id );
 
 	std::string live;
-	m_connection->read( live );
+	m_connection->read_line( live );
 	return live == "live";
 }
 
@@ -75,6 +89,6 @@ void shession_client_c::write_request( const std::string &request_type
 		, const std::string &session_id )
 {
 	std::string request( request_type +" "+ session_id );
-	m_connection->write( request );
+	m_connection->write_line( request );
 }
 
