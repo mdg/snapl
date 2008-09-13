872b69418a654beed3934e21761cdd3016f71bc2
Matt Graham <mdg149@optonline.net>
2008-09-13 00:18:18 -0400

    commented a debug message and added a backlog item to allow logging on and off

--- src/request_reader.cpp
+++ src/request_reader.cpp
@@ -67,7 +67,7 @@ request_c * request_reader_c::create_request( connection_i &conn )
 
 request_type_e request_reader_c::get_request_type( const std::string& req_type )
 {
-	std::cerr << "request = '" << req_type << "'\n";
+	// std::cerr << "request = '" << req_type << "'\n";
 
 	request_type_e rt( RT_NULL );
 	if ( req_type == "create" ) {
