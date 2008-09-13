387bda68356455b407ed68970a871c5b0e5d1e20
Matt Graham <mdg149@optonline.net>
2008-09-13 00:22:39 -0400

    removed token data from the request_c class

--- src/request.h
+++ src/request.h
@@ -34,18 +34,6 @@ public:
 	request_c( request_type_e, const std::string& session_id );
 
 	/**
-	 * Set the token name.  This is only for token requests.
-	 */
-	void set_token_name( const std::string& token_name )
-		{ m_token_name = token_name; }
-
-	/**
-	 * Set the token value.  This is optional.
-	 */
-	void set_token_value( const std::string& token_value )
-		{ m_token_value = token_value; }
-
-	/**
 	 * Get the type of this request
 	 */
 	request_type_e request_type() const { return m_req_type; }
@@ -55,21 +43,9 @@ public:
 	 */
 	const std::string & session_id() const { return m_session_id; }
 
-	/**
-	 * Get the token name.
-	 */
-	const std::string & token_name() const { return m_token_name; }
-
-	/**
-	 * Get the token value.
-	 */
-	const std::string & token_value() const { return m_token_value; }
-
 private:
 	request_type_e m_req_type;
 	std::string m_session_id;
-	std::string m_token_name;
-	std::string m_token_value;
 };
 
 
