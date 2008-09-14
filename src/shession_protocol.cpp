c31d325ef5dd9f98eb515214053b24c87b59a986
Matt Graham <mdg149@optonline.net>
2008-09-14 15:16:40 -0400

    added shession_protocol class for creating the protocols used in the shessiond server.  only the service protocol is implemented yet.  the mirror and admin protocols still need to be added.

---
+++ src/shession_protocol.cpp
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2008 Matthew Graham
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "shession_protocol.h"
+#include "request_processor.h"
+
+
+shession_protocol_c::shession_protocol_c( shession_store_i &store )
+: m_store( store )
+, m_service()
+, m_mirror()
+, m_admin()
+, m_create( new create_request_processor_c( store ) )
+, m_renew( new renew_request_processor_c( store ) )
+, m_kill( new kill_request_processor_c( store ) )
+, m_close( new close_request_processor_c( store ) )
+{}
+
+shession_protocol_c::~shession_protocol_c()
+{}
+
+
+protocol_c & shession_protocol_c::create_service_protocol( short port )
+{
+	m_service.reset( new protocol_c( port ) );
+	m_service->add( *m_create );
+	m_service->add( *m_renew );
+	m_service->add( *m_kill );
+	m_service->add( *m_close );
+	return *m_service;
+}
+
---
+++ src/shession_protocol.h
@@ -0,0 +1,47 @@
+#ifndef SHESSION_PROTOCOL_H
+#define SHESSION_PROTOCOL_H
+/**
+ * Copyright 2008 Matthew Graham
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "protocol.h"
+
+
+class shession_protocol_c
+{
+public:
+	shession_protocol_c( shession_store_i & );
+	~shession_protocol_c();
+
+	protocol_c & create_service_protocol( short service_port );
+	protocol_c & create_mirror_protocol( short mirror_port );
+	protocol_c & create_admin_protocol( short admin_port );
+
+private:
+	shession_store_i &m_store;
+
+	// protocols
+	std::auto_ptr< protocol_c > m_service;
+	std::auto_ptr< protocol_c > m_mirror;
+	std::auto_ptr< protocol_c > m_admin;
+
+	std::auto_ptr< request_processor_i > m_create;
+	std::auto_ptr< request_processor_i > m_renew;
+	std::auto_ptr< request_processor_i > m_kill;
+	std::auto_ptr< request_processor_i > m_close;
+};
+
+
+#endif
+
