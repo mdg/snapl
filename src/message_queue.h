e4c8887da434a929939e4b01a7c252826b05d340
Matt Graham <mdg149@optonline.net>
2008-12-04 20:05:15 -0500

    replaced message_queue_i with inbox_i and outbox_i.

--- src/message_queue.h
+++ src/message_queue.h
@@ -16,14 +16,21 @@
  */
 
 
-class message_queue_i
+class inbox_i
 {
 public:
-	virtual ~message_queue_i() {}
+	virtual ~inbox_i() {}
 
-	virtual void send( message_i * ) = 0;
-	virtual void send( const message_i & ) = 0;
-	virtual void receive( message_i * ) = 0;
+	virtual void send( connection_i &, message_i * ) = 0;
+	virtual void send( connection_i &, const message_i & ) = 0;
+};
+
+class outbox_i
+{
+public:
+	virtual ~outbox_i() {}
+
+	virtual void receive( connection_i & );
 };
 
 
