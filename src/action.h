0510815e61c7c53a0c5bedbd169e028981320d68
Matthew Graham <mdg149@optonline.net>
2008-12-19 19:25:14 -0500

    renamed action.process to be action.execute

--- src/action.h
+++ src/action.h
@@ -29,7 +29,7 @@ class action_i
 {
 public:
 	const std::string & request_type() const { return m_request_type; }
-	virtual void process( const request_c &, response_c & ) = 0;
+	virtual void execute( const request_c &, response_c & ) = 0;
 
 protected:
 	action_i( const std::string &req_type )
