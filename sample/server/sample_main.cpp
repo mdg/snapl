7d52ca5ef83551fb60bae0d2828e92bf71b8e4f4
Matthew Graham <mdg149@optonline.net>
2008-12-25 17:30:15 -0500

    implemented the sample_main so the listener actually turns on and runs

--- sample/server/sample_main.cpp
+++ sample/server/sample_main.cpp
@@ -1,11 +1,20 @@
 #include "sample_protocol.h"
 #include "../../src/dispatcher.h"
+#include "../../src/connection_acceptor.h"
+#include "../../src/polling_server_queue.h"
 
 
 int main( int argc, char **argv )
 {
-	sample_protocol_c sample_protocol( 9000 );
-	// dispatcher_c dispatch;
+	short port( 9000 );
+
+	sample_protocol_c sample_protocol( port );
+	connection_acceptor_c acceptor;
+	acceptor.open_listener( port );
+	polling_server_queue_c queue( acceptor );
+	dispatcher_c dispatch( queue );
+
+	dispatch.main_loop();
 	return 0;
 }
 
