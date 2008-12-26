29aa0369e9c78f36e1dcec27f6b115e27d630b31
Matthew Graham <mdg149@optonline.net>
2008-12-26 11:12:03 -0500

    designed a new class structure that will have a better application interface.  it more easily shares the request/response between client & server.

--- test/client_usage_test.h
+++ test/client_usage_test.h
@@ -23,3 +23,58 @@ transmission
 transfer
 request
 
+class get_request_c
+: public request_c
+{
+public:
+	void copy( copy_stream_i &s )
+	{
+		s.copy( m_user_id );
+		s.ocopy( m_session_id );
+	}
+};
+
+class get_response_c
+: public response_c
+{
+public:
+	void copy( copy_stream_i &s )
+	{
+		s.copy( m_code );
+		s.copy( m_response );
+	}
+};
+
+class service_i
+{
+	virtual void execute() = 0;
+};
+
+template < typename ReqT, typename Respt >
+class service_c
+{
+public:
+	virtual void execute()
+	{
+		execute( m_request, m_response );
+	}
+};
+
+class get_service_c
+: service_c< get_request_c, get_response_c >
+{
+public:
+	void execute( const get_request_c &req, get_rsponse_c &resp )
+	{
+		resp.ok( m_code );
+	}
+};
+
+template < get_request_c, get_response_c >
+class get_command_c
+{
+public:
+	get_command_c( const std::string &user_id
+			, const std::string &session_id );
+};
+
