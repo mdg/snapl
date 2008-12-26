ff9844cb5be179341047971570305a318a6b349f
Matthew Graham <mdg149@optonline.net>
2008-12-26 15:28:55 -0500

    updated the client_usage_test file w/ message class, message import & export, and how it would work with requests & responses

--- test/client_usage_test.h
+++ test/client_usage_test.h
@@ -29,17 +29,142 @@ transmission
 transfer
 request
 
+
+class message_c
+{
+public:
+	message_c();
+	void add_arg( const std::string & );
+	void add_content( const std::string & );
+
+	int argc() const;
+	const std::string & argv( int i ) const;
+
+	const std::list< std::string > & content() const;
+	std::list< std::string >::const_iterator begin_content() const;
+	std::list< std::string >::const_iterator end_content() const;
+
+private:
+};
+
+
+/**
+ * Object that exports general values from a message class into a typed
+ * object.
+ */
+class message_export_c
+{
+public:
+	message_export_c( const message_c &msg )
+	: m_message( msg )
+	, m_arg( 0 )
+	, m_error( false )
+	{}
+
+	template < typename T >
+	message_export_c & operator + ( T &value )
+	{
+		// check that too many values haven't already been read
+		if ( m_arg >= m_message.argc() ) {
+			m_error = true;
+			return *this;
+		}
+
+		copy( value, m_message.argv[ m_arg++ ] );
+		return *this;
+	}
+
+	/**
+	 * Copy a string value to a typed value.
+	 */
+	template < typename T >
+	void copy( T &dest, const std::string &src )
+	{
+		std::istringstream in( src );
+		in >> dest;
+		if ( in.error() ) {
+			m_error = true;
+		}
+	}
+
+private:
+	message_c &m_msg;
+	int m_arg;
+	bool m_error;
+};
+
+
+/**
+ * Object that imports values from a typed object into a string-based message.
+ */
+class message_import_c
+{
+public:
+	message_import_c( message_c &msg )
+	: m_message( msg )
+	, m_arg( 0 )
+	, m_error( false )
+	{}
+
+	template < typename T >
+	copy_ostream & operator + ( const T &value )
+	{
+		std::string arg;
+		copy( arg, value );
+		out >> value;
+		return *this;
+	}
+
+	/**
+	 * Copy a value into a string type.
+	 */
+	template < typename T >
+	void copy( std::string &arg, const T &value )
+	{
+		std::ostringstream out;
+		out << value;
+		arg = out.str();
+	}
+
+private:
+	std::ostream &m_out;
+	int m_arg;
+	bool m_error;
+};
+
+
 class get_request_c
 : public request_c
 {
 public:
+	/**
+	 * Command constructor.
+	 */
 	get_request_c( const std::string &user_id
 			, const std::string &session_id );
+	: request_c( "get" )
+	, m_user_id( user_id )
+	, m_session_id( session_id )
+	{}
 
-	void copy( copy_stream_i &s )
+	/**
+	 * Service constructor.
+	 */
+	get_request_c( const request_c &req )
+	: request_c( req )
+	, m_user_id( req.argv< int >( 0 ) )
+	, m_session_id( req.argv< std::string >( 1 ) )
+	{}
+
+	/**
+	 * Copy values of the request.
+	 */
+	template < typename T >
+	void copy( T &stream )
 	{
-		s.copy( m_user_id );
-		s.ocopy( m_session_id );
+		stream + m_user_id + m_session_id;
+		// alternative usage.  are | better?
+		// stream | m_user_id | m_session_id;
 	}
 
 private:
@@ -51,10 +176,10 @@ class get_response_c
 : public response_c
 {
 public:
-	void copy( copy_stream_i &s )
+	template < typename T >
+	void copy( T &stream )
 	{
-		s.copy( m_code );
-		s.copy( m_response );
+		stream + m_code + m_response;
 	}
 };
 
