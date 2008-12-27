1a39ecaeb196777a52a22df410a251cd66abe3a0
Matthew Graham <mdg149@optonline.net>
2008-12-27 00:36:35 -0500

    moved message import & export into the message file

--- src/message.h
+++ src/message.h
@@ -39,5 +39,91 @@ private:
 };
 
 
+
+/**
+ * Object that exports general values from a message class into a typed
+ * object.
+ */
+class message_export_c
+{
+public:
+	message_export_c( const message_c &msg )
+	: m_message( msg )
+	, m_arg( 0 )
+	, m_error( false )
+	{}
+
+	template < typename T >
+	message_export_c & operator + ( T &value )
+	{
+		// check that too many values haven't already been read
+		if ( m_arg >= m_message.argc() ) {
+			m_error = true;
+			return *this;
+		}
+
+		copy( value, m_message.argv[ m_arg++ ] );
+		return *this;
+	}
+
+	/**
+	 * Copy a string value to a typed value.
+	 */
+	template < typename T >
+	void copy( T &dest, const std::string &src )
+	{
+		std::istringstream in( src );
+		in >> dest;
+		if ( in.error() ) {
+			m_error = true;
+		}
+	}
+
+private:
+	message_c &m_msg;
+	int m_arg;
+	bool m_error;
+};
+
+
+/**
+ * Object that imports values from a typed object into a string-based message.
+ */
+class message_import_c
+{
+public:
+	message_import_c( message_c &msg )
+	: m_message( msg )
+	, m_arg( 0 )
+	, m_error( false )
+	{}
+
+	template < typename T >
+	copy_ostream & operator + ( const T &value )
+	{
+		std::string arg;
+		copy( arg, value );
+		out >> value;
+		return *this;
+	}
+
+	/**
+	 * Copy a value into a string type.
+	 */
+	template < typename T >
+	void copy( std::string &arg, const T &value )
+	{
+		std::ostringstream out;
+		out << value;
+		arg = out.str();
+	}
+
+private:
+	std::ostream &m_out;
+	int m_arg;
+	bool m_error;
+};
+
+
 #endif
 
