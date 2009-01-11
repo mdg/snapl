22025fab33206af7ba78abd34153cd72e71d4e01
Matthew Graham <mdg149@optonline.net>
2009-01-10 19:22:18 -0500

    fixed compile error where the mock_request was using the old + operator instead of the proper << operator

--- test/request_test.h
+++ test/request_test.h
@@ -27,7 +27,7 @@ public:
 	, m_id()
 	, m_number( 0 )
 	{
-		m_arg + m_id + m_number;
+		m_arg << m_id << m_number;
 	}
 
 	mock_request_c( const std::string &id, int number )
@@ -35,7 +35,7 @@ public:
 	, m_id( id )
 	, m_number( number )
 	{
-		m_arg + m_id + m_number;
+		m_arg << m_id << m_number;
 	}
 
 	template < typename T >
