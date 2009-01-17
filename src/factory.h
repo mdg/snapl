4ddce2738a32acaf0fe5e9a5db99a50412bd9f36
Matthew Graham <mdg149@optonline.net>
2009-01-17 00:16:13 -0500

    added the a templated factory class
    it creates implementations of base type factories

---
+++ src/factory.h
@@ -0,0 +1,50 @@
+#ifndef FACTORY_H
+#define FACTORY_H
+/**
+ * Copyright 2008 Matthew Graham
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+template < typename I >
+class factory_i
+{
+public:
+	/**
+	 * Empty virtual constructor so subclasses will have things deleted.
+	 */
+	virtual ~factory_i() {}
+
+	/**
+	 * Pure virtual for creating objects of the given interface type.
+	 */
+	virtual I * create() const = 0;
+};
+
+
+template < typename C, typename I >
+class factory_c
+: public factory_i< I >
+{
+public:
+	virtual ~factory_c() {}
+
+	virtual I * create() const
+	{
+		return new C();
+	}
+};
+
+
+#endif
+
