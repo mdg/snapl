25eba722b0b6621dcac1c896b84f60a91fd26109
Matthew Graham <mdg149@optonline.net>
2009-02-15 23:37:07 -0500

    added virtual overrides to the mock conn listener

--- test/connection_listener_test.h
+++ test/connection_listener_test.h
@@ -27,10 +27,14 @@ public:
 	{}
 	virtual ~mock_connection_listener_c() {}
 
+	virtual bool listen( short port ) { return true; }
+
 	virtual connection_i * connection()
 	{
 		return m_connection.line_ready() ? &m_connection : NULL;
 	}
+
+	virtual void replace( connection_i * ) {}
 
 private:
 	connection_i &m_connection;
