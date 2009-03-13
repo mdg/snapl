ae36e29db99c29a72d1cb13426f4825f652ec8b7
Matthew Graham <mdg149@optonline.net>
2009-03-13 09:27:40 -0400

    new libevent based listener & connection classes
    not implemented yet

---
+++ include/snapl/net/event_listener.h
@@ -0,0 +1,55 @@
+#ifndef SNAPL_EVENT_LISTENER_H
+#define SNAPL_EVENT_LISTENER_H
+/**
+ * Copyright 2008 Matthew Graham
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "net/connection_listener.h"
+
+
+/**
+ * An abstract class that looks for and handles connections.
+ */
+class event_listener_c
+: public connection_listener_i
+{
+public:
+	/**
+	 * Virtual destructor.
+	 */
+	virtual ~connection_listener_i();
+
+	/**
+	 * Set this listener to listen on a given port.
+	 */
+	virtual bool listen( short port );
+
+	/**
+	 * Get an open connection from the listener that is ready
+	 * to be read.  Callers should expect that this may block
+	 * while waiting for a connection to become ready.
+	 * @return an instance of a connection_i.
+	 */
+	virtual connection_i * connection();
+
+	/**
+	 * Replace an open connection to be listened to again now that it's
+	 * no longer in use.
+	 */
+	virtual void replace( connection_i * );
+};
+
+
+#endif
+
