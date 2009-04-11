be38323fc9a42ce9e826663e05897de7036c555c
Matthew Graham <mdg149@optonline.net>
2009-04-11 16:24:00 -0400

    experimental protocol mapping in usage test file

--- test/client_usage_test.h
+++ test/client_usage_test.h
@@ -117,3 +117,83 @@ public:
 	{}
 };
 
+class protocol_mapping_i
+{
+public:
+	virtual ~protocol_mapping() {}
+
+	virtual const std::string & name() const = 0;
+	virtual bool command_type( command_i & ) const = 0;
+	virtual bool action_type( action_i & ) const = 0;
+};
+
+
+class core_protocol_c
+: public protocol_i
+{
+public:
+	core_protocol_c()
+	{
+		bind< get_command_c, get_action_c >( "get" );
+		bind< add_command_c, add_action_c >( "add" );
+		bind< up_command_c, up_action_c >( "up" );
+		bind< del_command_c, del_action_c >( "del" );
+	}
+
+	core_protocol_c()
+	{
+		bind< get_command_c >( "get", m_get );
+		bind< add_command_c >( "add", m_add );
+		bind< up_command_c >( "up", m_up );
+		bind< del_command_c >( "del", m_del );
+	}
+
+	core_protocol_c( T &binder )
+	{
+		binder.bind< get_command_c, get_action_c >( "get" );
+		binder.bind< add_command_c, add_action_c >( "add" );
+	}
+
+	void bind( T &binder )
+	{
+		binder.bind< get_command_c, get_action_c >( "get" );
+		binder.bind< add_command_c, add_action_c >( "add" );
+	}
+
+	BIND_COMMAND( get );
+	BIND_COMMAND( add );
+	BIND_COMMAND( up );
+
+	template < typename T >
+	std::string name( const T & ) const { return std::string(); }
+
+
+	void bind1( protocol_map_i &pmap )
+	{
+		pmap.bind< get_command_c, get_action_c >( "test" );
+		pmap.bind< get_command_c >( "test" ).bind< get_action_c >();
+		pmap("test").bind< get_command_c >().bind< get_action_c >();
+		pmap("test").bind< get_command_c, get_action_c >();
+		bind("test").to< get_command_c, get_action_c >();
+	}
+
+	void bind2( protocol_binding_i &binding )
+	{
+		pmap.bind< get_command_c, get_action_c >( "test" );
+		pmap.bind< get_command_c >( "test" ).bind< get_action_c >();
+		pmap("test").bind< get_command_c >().bind< get_action_c >();
+		pmap("test").bind< get_command_c, get_action_c >();
+		bind("test").to< get_command_c, get_action_c >();
+		binding.bind< get_command_c, get_action_c >( "get" );
+	}
+
+	void map2( protocol_map_i &pmap )
+	{
+		pmap.set< get_command_c >( "test", m_get_action );
+	}
+
+	template < typename CommandT >
+	const std::string & name() const;
+
+};
+
